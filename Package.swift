2d7aa6899c7ad3136fe7d74d78d49e0eaf1bbfcd
François Devémy <61689139+FrancoisDevemy5@users.noreply.github.com>
2024-07-19 19:15:03 +0200

    [CA-1916] Support de Swift Package Manager (#1)

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Reach5Facebook",
+    products: [
+        .library(name: "Reach5Facebook", targets: ["Reach5Facebook"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", .upToNextMajor(from: "17.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "Reach5Facebook",
+            dependencies: [
+                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
+                .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
+            ],
+            path: "IdentitySdkFacebook"),
+    ]
+)
--- Podfile
+++ Podfile
@@ -3,6 +3,6 @@ use_frameworks!
 platform :ios, '13.0'
 
 target 'IdentitySdkFacebook' do
-  pod 'IdentitySdkCore', :path => '../'
-  pod 'IdentitySdkFacebook', :path => '../'
+  pod 'IdentitySdkCore'
+  pod 'IdentitySdkFacebook', :path => 'IdentitySdkFacebook.podspec'
 end
