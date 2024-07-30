58de1f5b63b402f902ca25baca3e80dd65aa9338
François Devémy <francois.devemy@reach5.co>
2024-07-31 01:45:28 +0200

    Modif du header

--- Reach5Facebook/Reach5Facebook.h
+++ Reach5Facebook/Reach5Facebook.h
@@ -1,9 +1,9 @@
 #import <UIKit/UIKit.h>
 
-//! Project version number for IdentitySdkFacebook.
-FOUNDATION_EXPORT double IdentitySdkFacebookVersionNumber;
+//! Project version number for Reach5Facebook
+FOUNDATION_EXPORT double Reach5FacebookVersionNumber;
 
-//! Project version string for IdentitySdkFacebook.
-FOUNDATION_EXPORT const unsigned char IdentitySdkFacebookVersionString[];
+//! Project version string for Reach5Facebook.
+FOUNDATION_EXPORT const unsigned char Reach5FacebookVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <IdentitySdkFacebook/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <Reach5Facebook/PublicHeader.h>
